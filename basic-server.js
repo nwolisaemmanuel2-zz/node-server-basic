b4f5514da2ae4ae960f50b3f9c1b0fbaef9e9d0e
Nwolisa Emmanuel Ekene <47080616+nwolisaemmanuel2@users.noreply.github.com>
2020-05-09 15:32:04 +0000

    server.js

---
+++ basic-server.js
@@ -0,0 +1,25 @@
+// Basic Node Server
+
+const http = require("http");
+
+const server = http.createServer(function(req, res){
+
+    res.setHeader("Content-type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.writeHead(200); //status code HTTP 200 / OK
+
+    let dataObj = {
+        id: 11,
+        name: "Nwolisa Emmanuel Ekene",
+        email: "nwolisaemmanuel2@gmail.com",
+        age: 29,
+        location: "Techiman"
+    };
+    let data = JSON.stringify(dataObj);
+    res.end(data)
+});
+
+
+server.listen(9999, function(){
+    console.log('listening on port 9999');
+});
